31b15462fd12381ca2de1501c6e2828becbb4fe3
BuildTools <unconfigured@null.spigotmc.org>
2022-10-12 18:18:28 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# SSB-IslandSizePermission
+
+This’s a addon of SuperiorSkyblock2 plugin.
+
+Permission usage: `superior.island.size.<size>`
---
+++ src/main/java/me/tptuaasn/IslandSize.java
@@ -0,0 +1,50 @@
+package me.tptuaasn;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.bgsoftware.superiorskyblock.api.events.IslandCreateEvent;
+import com.bgsoftware.superiorskyblock.api.island.Island;
+import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
+
+public class IslandSize extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onIslandCreate(IslandCreateEvent e) {
+		final SuperiorPlayer superiorPlayer = e.getPlayer();
+		final Island island = superiorPlayer.getIsland();
+		int islandSize = SuperiorSkyblockAPI.getSettings().getDefaultValues().getIslandSize();
+		final int islandMaxSize = SuperiorSkyblockAPI.getSettings().getMaxIslandSize();
+
+		for (String s : getValues(superiorPlayer.asPlayer())) {
+			int size = s.matches("^[0-9]+$") ? Integer.valueOf(s) : islandSize;
+			islandSize = Math.max(islandSize, size);
+		}
+
+		if (islandSize % 2 != 0) islandSize--;
+		if (islandSize > islandMaxSize) islandSize = islandMaxSize;
+		island.setIslandSize(islandSize);
+	}
+
+	private List<String> getValues(Player p) {
+		return Stream.of(p.getEffectivePermissions()).flatMap(Collection::stream)
+				.flatMap(s -> Arrays.stream(s.getPermission().split("superior.island.size.")))
+				.collect(Collectors.toList());
+	}
+}
